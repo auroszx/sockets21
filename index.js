c9484e9d6d7a982a24420589432860247809f056
Andrés Hernández <andres.hernandez@emqu.net>
2018-08-02 22:49:18 -0400

    First version release candidate 1. Gameplay functionality is being tested and a basic UI has been implemented.

--- index.js
+++ index.js
@@ -5,12 +5,25 @@ const config = require('./helpers/config');
 app.use('/views', express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/node_modules'));
 
-app.get('/',(req,res)=>{
+app.get('/',(req,res) => {
 	// res.sendFile(__dirname + '/index.html');
 	res.redirect('views/index.html');
 });
 
-var players;
+var players = 0;
+var passes = 0;
+var wins =[
+	{
+		player: "",
+		score: 0,
+		total: 0
+	},
+	{
+		player: "",
+		score: 0,
+		total: 0
+	}
+	];
 
 //An array of player names and drawn cards Good enough to build "hands".
 var hands = [];
@@ -39,7 +52,9 @@ function generateCardDeck() {
 
 function getRandomCard() {
 	//return deck.splice(Math.floor((Math.random() * 52) + 1), 1);
-	return deck.splice(Math.floor((Math.random() * deck.length) + 1), 1);
+	var crap = deck.splice(Math.floor((Math.random() * deck.length) + 1), 1);
+	return crap;
+	//return deck.splice(Math.floor((Math.random() * deck.length) + 1), 1);
 }
 
 
@@ -51,36 +66,84 @@ const io = require('socket.io').listen(server);
 console.log("Sockets21 NodeJS Server is running...");
 
 io.sockets.on('connection', function(socket) {
-
-	io.clients((error, clients) => {
-		if (error) {
-			console.log(error);
-		}
-		players = clients;
-	});
+	players++;
+	socket.emit("gameLog", "You joined the game");
 	socket.broadcast.emit('gameLog', "Player joined");
+	socket.emit("gameLog", "There are "+players+" connected players");
+	generateCardDeck();
 
 	//Game stuff
 
-	socket.on('playerDraws', function(player) {
+	socket.on('playerDraws', (player) => {
+		passes = 0;
 		socket.broadcast.emit('gameLog', "Player "+player+" drew a card");
-		var card = getRandomCard();
-		hands.push({player: player, card: card});
-		console.log("Player "+player+" drawed: "+card);
-      	socket.emit('gameLog', card);
-   	
+		var dcard = getRandomCard();
+		hands.push({player: player, card: dcard});
+		socket.emit("gameLog", "You drew a card");
+		socket.broadcast.emit("enemyCardDrawn");
+      	socket.emit("cardDrawn", JSON.stringify(dcard));
+      	socket.emit("turnLock", true);
+      	socket.broadcast.emit("turnLock", false);
 	});
 
 	socket.on('playerPasses', function(player) {
+		passes++;
 		socket.broadcast.emit('gameLog', "Player "+player+" passed");
-	});
+		socket.emit("gameLog", "You passed");
+		socket.emit("turnLock", true);
+      	socket.broadcast.emit("turnLock", false);
+      	if (passes == players) {
+      		io.emit("handReset");
+      		generateCardDeck();
 
-	socket.on("showCards", function() {
+      		console.log(hands);
+      		for (var i in hands) {
+      			if (hands[i].player != wins[0].player && hands[i].player != wins[1].player && wins[0].player == "") {
+      				wins[0].player = hands[i].player;
+      				wins[0].score += hands[i].card[0].value;
+      			}
+      			else if (hands[i].player != wins[0].player && hands[i].player != wins[1].player && wins[0].player != "") {
+      				wins[1].player = hands[i].player;
+      				wins[1].score += hands[i].card[0].value;
+      			}
+      			else if (hands[i].player == wins[0].player && hands[i].player != wins[1].player) {
+      				wins[0].score += hands[i].card[0].value;
+      			}
+      			else if (hands[i].player != wins[0].player && hands[i].player == wins[1].player) {
+      				wins[1].score += hands[i].card[0].value;
+      			}
+      		}
 
+      		if (wins[0].score > wins[1].score && wins[0].score <= 21) {
+      			wins[0].total++;
+      		}
+      		else if (wins[0].score < wins[1].score && wins[1].score <= 21) {
+      			wins[1].total++;
+      		}
+
+      		if (wins[0].total == 3 || wins[1].total == 3) {
+      			var winner = "";
+      			if (wins[0].total > wins[1].total) {
+      				winner = wins[0].player;
+      			}
+      			else {
+      				winner = wins[1].player;
+      			}
+      			io.emit("gameOver", wins[0].player+": "+wins[0].total+" | "+wins[1].player+": "+wins[1].total+"\nThe winner is: "+winner);
+      		}
+
+      		wins[0].score = 0;
+      		wins[1].score = 0;
+
+      		console.log(wins);
+      		io.emit("gameStats", wins);
+      		passes = 0;
+      	}
 	});
 
 	socket.on('playerDisconnect', function(player) {
-		socket.broadcast.emit('gameLog', "Player quit");
+		players--;
+		socket.broadcast.emit('gameLog', "Player "+player+" quit");
 	});
 
 
--- public/js/main.js
+++ public/js/main.js
@@ -1,40 +1,116 @@
 //Client JS for Sockets21
 
-let socket = io('http://localhost:3000');
-
 function $(id) {
     return document.getElementById(id);
 }
 
+var deckdraw_allowed = true;
 
-function joinGame() {
-    let socket = io('http://localhost:3000');
+//Game events
+let socket = io('http://localhost:3000');
 
-    socket.on('playerJoined', () => {
-        console.log("Player joined");
-    });
+socket.on('playerJoined', () => {
+    console.log("Player joined");
+});
 
-    socket.on('playerDisconnect', (player) => {
-        console.log("Player disconnected");
-    });
+socket.on('playerDisconnect', (player) => {
+    console.log("Player disconnected");
+});
 
-    socket.on("gameLog", (msg) => {
-        console.log(msg);
-    });
+socket.on("gameLog", (msg) => {
+    console.log(msg);
+    $("gameLog").innerHTML += "<p>"+msg+"</p>";
+    $("gameLog").scrollTop = $("gameLog").scrollHeight;
+});
 
+socket.on("cardDrawn", (dcard) => {
+    var card = JSON.parse(dcard)[0];
+    console.log(card);
+    var cardimg = document.createElement("img");
+    cardimg.src = "images/cards/"+card.value+card.suit+".png";
+    cardimg.style = "width: 10%; height: 10%; margin-top: 5px; margin-left: 5px; margin-right: 5px; border-style: solid; border-width: 2px;";
+    $("playerhand").appendChild(cardimg);
+});
+
+socket.on("enemyCardDrawn", () => {
+    var enemycard = document.createElement("img");
+    enemycard.src = "images/cards/Back.png";
+    enemycard.style = "width: 10%; height: 10%; margin-top: 5px; margin-left: 5px; margin-right: 5px; border-style: solid; border-width: 2px;";
+    $("enemyhand").appendChild(enemycard);
+});
+
+socket.on("gameStats", (wins) => {
+    console.log(wins);
+});
+
+socket.on("turnLock", (status) => {
+    console.log(status);
+    $("drawbtn").disabled = status;
+    $("passbtn").disabled = status;
+    deckdraw_allowed = !status;
+});
+
+socket.on("handReset", () => {
+    handReset();
+});
+
+socket.on("gameOver", (msg) => {
+    alert(msg);
+    handReset();
+    scoreReset();
+});
+
+//Some game functions
+function renderDeck() {
+    for (var i = 1; i <= 10; i++) {
+        var deckcard = document.createElement("img");
+        deckcard.src = "images/cards/Back.png";
+        deckcard.style = "border-style: solid; border-width: 1px; transform: rotate(90deg); margin-left: 50%; margin-top: 30%; width: 10%; height: 10%; position: absolute; top: "+(i*5)+"px; left: "+(-i*5)+"px; z-index: "+i+"px;";
+        $("deck").appendChild(deckcard);
+    }
+}
+
+function drawFromDeck() {
+    if (deckdraw_allowed) {
+        drawCard();
+    }
 }
 
 function drawCard() { 
-    socket.emit('playerDraws', $("user").value);
+    socket.emit('playerDraws', $('player').value.trim()===''? '2' :$('player').value);
 }
 
 function pass() {
-    socket.emit('playerPasses', $("user").value);
+    socket.emit('playerPasses', $('player').value.trim()===''? '2' :$('player').value);
+}
+
+function clearLog() {
+    while ($("gameLog").firstChild) {
+        $("gameLog").removeChild($("gameLog").firstChild);
+    }
+}
+
+function handReset() {
+    while ($("playerhand").firstChild) {
+        $("playerhand").removeChild($("playerhand").firstChild);
+    }
+    while ($("enemyhand").firstChild) {
+        $("enemyhand").removeChild($("enemyhand").firstChild);
+    }
+}
+
+function scoreReset() {
+    $("wins").innerHTML = "";
 }
 
 
-//Closing the window counts as a player disconnect
+//Closing the window or reloading counts as a player disconnect
 window.onunload = () => {
-    //socket.emit('playerDisconnect', $('user').value.trim()===''? 'Anonimo' :$('user').value);
-    socket.emit('playerDisconnect', $('user').value);
+    socket.emit('playerDisconnect', $('player').value.trim()===''? '2' :$('player').value);
+    //socket.emit('playerDisconnect', $('player').value);
+}
+
+window.onbeforeunload = () => {
+    socket.emit('playerDisconnect', $('player').value.trim()===''? '2' :$('player').value);
+    //socket.emit('playerDisconnect', $('player').value);
 }
